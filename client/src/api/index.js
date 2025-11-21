8a5e29c54f477d31b02817978f146fcb48bede6e
Akinkuolie Olaitan <olaitanclems@gmail.com>
2025-11-21 20:16:50 +0100

    Update API base URL to include '/api' path

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://mentorconnect-backend-o5mm.onrender.com/",
+  baseURL: "https://mentorconnect-backend-o5mm.onrender.com/api",
   withCredentials: true,
 });
 
